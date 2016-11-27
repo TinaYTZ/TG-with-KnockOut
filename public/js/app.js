879d16c57f49f82d5bd80088f487eb42bd6d9f3f
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-11-26 21:12:03 -0800

    KnockOut data bind working

--- public/js/app.js
+++ public/js/app.js
@@ -2,116 +2,143 @@
 
 /* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 /*global console:true */
-var main = function () { 
+
   'use strict';
 var socket= io.connect();
+
 var $userForm=$('#userForm');
-var $username=$('#username'); 
-var $userFormArea=$('#userFormArea');
+//var $username=$('#username'); 
 var userId;
 var $dataArea=$('#dataArea');
 
+var users=[];
 
 
-$('#mainArea').hide();
 
 var answerId;
 var text = '';
-var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-for( var i=0; i < 10; i++ )
-      {  text += possible.charAt(Math.floor(Math.random() * possible.length));}
-userId = text;
+// var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// for( var i=0; i < 10; i++ )
+//       {  text += possible.charAt(Math.floor(Math.random() * possible.length));}
+// userId = text;
+
 
 socket.on('Question', function(data){
-  $('#QAArea').show();
-//  $('#questionForm').hide();
-  $('#answerQA').removeAttr('disabled');
-  $('#submitAnswer').removeAttr('disabled');
-  $('#answerQA').val('');
+  //$('#answerQA').removeAttr('disabled');
+  //$('#submitAnswer').removeAttr('disabled');
   var html=data.question;
   answerId=data.answerId;
   console.log('answerid',answerId);
   $dataArea.html(html);
-
- 
   });
-$('#getQestion').click(function(e){
-  e.preventDefault();
-  socket.emit('new-round');
-  $('#answerQA').removeAttr('disabled');
-  $('#submitAnswer').removeAttr('disabled');
-  $('#answerQA').val('');
-
-  document.getElementById('check-answer').innerHTML = '';
-  document.getElementById('history').innerHTML = '';
-
-});
 
 socket.on('get users', function(data){
-    var html='';
-    console.log('user'+data);
-    for (var i = 0; i < data.length; i++) {
-      html+='<p>' + data[i]+ '</p>';
+    users=[];
+   for (var i = 0; i < data.length; i++) {
+    users.push(new userItem(data[i]));
   }
-  $('#userList').html(html);
+    viewmodel.userList(users);
+  
   });
-
-
-
- $userForm.submit(function(e){
-      e.preventDefault();
-      userId=$username.val();
-      socket.emit('new user', $username.val(),function(data){
-        if(data){
-          $userFormArea.hide();
-          $('#mainArea').show();
-        }
-        else{
-        }
-      });
-      $username.val('');
-  });
-
-
-$('#submitAnswer').click(function(e){
-  $('#submitAnswer').attr('disabled', 'disabled');
-   $('#answerQA').attr('disabled', 'disabled');
-  e.preventDefault();
-  console.log($('#answerQA').val());
-  socket.emit('submit-answer',{
-        'answerId': answerId,
-        'answer': $('#answerQA').val(),
-        'userId': userId
-    });
-});
 
 
 socket.on('check-answer', function(data){
-  var html='<p>answer:'+data+'<p>';
-  console.log('check-answer data', data);
-  $('#check-answer').html(html);
-  $('#check-answer').fadeIn(2000);
+  var text='answer:'+data;
+  viewmodel.checkAnswer(text);
   });
 
-socket.on('score',function(data){
-    var html='';
-    for (var i = 0; i < data.length; i++) {
-     // html+='<li  class='bg-info' >'+ data[i] +'</li>';
-     html+='<p>user:'+data[i].userId+'&nbsp &nbsp &nbsp'+data[i].correct+'</p>';
-  }
 
-  console.log(html);
-    $('#leadboard').html(html);
+
+socket.on('score',function(data){
+    console.log("score"+JSON.stringify(data));
+    var record=[];
+    for (var i = 0; i < data.length; i++) {
+     var text=data[i].userId+"    "+data[i].correct;
+     var item=new leadboardItem(text);
+     record.push(item);     
+     }
+     viewmodel.leadboardList(record);
 
 
 });
- socket.on('history', function(data){
-         console.log('history',JSON.stringify(data));
-         var html= '<p>'+JSON.stringify(data)+'</p>';
-         $('#history').append(html);
+ 
+  socket.on('history', function(data){
+    console.log('history',JSON.stringify(data));  
+     var item=new historyItem(JSON.stringify(data));  
+     var array=viewmodel.historyList();
+     array.push(item);
+     viewmodel.historyList(array);
+  });
 
- });
 
+
+function userItem(name) {
+    var self = this;
+    self.name = name;
+}
+
+function leadboardItem(record){
+      var self = this; 
+      self.item = record;
+}
+
+function historyItem(record) {
+    var self = this;
+    self.record = record;
+}
+
+
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+var  viewmodel= {
+    userName :ko.observable(''),
+    answerQA:ko.observable(''),
+    checkAnswer:ko.observable(''),
+    
+    historyList:ko.observableArray(),
+    leadboardList:ko.observableArray([]),
+    userList:ko.observableArray([]),
+    userFormArea:ko.observable(false),
+
+    historyArea:ko.observable(false),
+    mainArea:ko.observable(true),
+    checkAnswerArea:ko.observable(false),
+
+    addUser: function() {
+            console.log("new user's Name", viewmodel.userName());
+            socket.emit('new user', viewmodel.userName(),function(data){
+            //data true or false
+            if(data){
+                viewmodel.userFormArea(false);
+                viewmodel.mainArea(true);
+            }else{
+            }
+          });
+        },
+    submitAnswer:function() {
+        console.log("anserQA",this.answerQA());
+        socket.emit('submit-answer',{
+        'answerId': answerId,
+        'answer': this.answerQA(),
+        'userId': this.userName()
+    });
+        this.historyArea(true);
+        this.checkAnswerArea(false);
+    },
+    getQestion:function(){
+        socket.emit('new-round');
+        this.answerQA('');
+        this.checkAnswerArea(false)
+        this.historyArea(false);
+
+    }
+
+
+
+    
 };
-   
-    $(document).ready(main);
+viewmodel.userFormArea(true);
+viewmodel.mainArea(false);
+
+// Activates knockout.js
+ko.applyBindings(viewmodel);
+
