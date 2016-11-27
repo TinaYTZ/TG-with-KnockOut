c7b9637ebe8cb3ad05bb939b30e11d8d8a83f8cc
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-11-27 15:29:36 -0800

    valid html and js

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,95 @@
-/* jslint maxlen: 500 */
+/* jslint maxlen: 5000 */
 
 /* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
-/*global console:true */
+/*global console:true, io:true, ko:true*/
 
-  'use strict';
+'use strict';
+
 var socket= io.connect();
-
-var $userForm=$('#userForm');
-//var $username=$('#username'); 
-var userId;
-var $dataArea=$('#dataArea');
-
 var users=[];
-
-
-
 var answerId;
-var text = '';
-// var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-// for( var i=0; i < 10; i++ )
-//       {  text += possible.charAt(Math.floor(Math.random() * possible.length));}
-// userId = text;
 
+function UserItem(name) {
+    var self = this;
+    self.name = name;
+}
+
+function LeadboardItem(record){
+      var self = this; 
+      self.item = record;
+}
+
+function HistoryItem(record) {
+    var self = this;
+    self.record = record;
+}
+
+var  viewmodel= {
+    userName :ko.observable(''),
+    answerQA:ko.observable(''),
+    checkAnswer:ko.observable(''),
+    question:ko.observable(''),
+    historyList:ko.observableArray(),
+    leadboardList:ko.observableArray([]),
+    userList:ko.observableArray([]),
+    userFormArea:ko.observable(false),
+
+    historyArea:ko.observable(false),
+    mainArea:ko.observable(true),
+    checkAnswerArea:ko.observable(false),
+
+    addUser: function() {
+            socket.emit('new user', viewmodel.userName(),function(data){
+            //data true or false
+            if(data){
+                viewmodel.userFormArea(false);
+                viewmodel.mainArea(true);
+            }else{
+            }
+          });
+        },
+    submitAnswer:function() {
+        viewmodel.historyList.removeAll();
+
+        console.log('anserQA',this.answerQA());
+        socket.emit('submit-answer',{
+        'answerId': answerId,
+        'answer': this.answerQA(),
+        'userId': this.userName()
+    });
+        this.historyArea(true);
+        this.checkAnswerArea(false);
+    },
+    getQestion:function(){
+        socket.emit('new-round');
+        this.answerQA('');
+        this.checkAnswerArea(false);
+        this.historyArea(false);
+        viewmodel.historyList.removeAll();
+
+
+    }
+
+
+
+    
+};
+viewmodel.userFormArea(true);
+viewmodel.mainArea(false);
 
 socket.on('Question', function(data){
   //$('#answerQA').removeAttr('disabled');
   //$('#submitAnswer').removeAttr('disabled');
-  var html=data.question;
+  viewmodel.question(data.question);
   answerId=data.answerId;
   console.log('answerid',answerId);
-  $dataArea.html(html);
   });
 
 socket.on('get users', function(data){
+    viewmodel.userList.removeAll();
     users=[];
    for (var i = 0; i < data.length; i++) {
-    users.push(new userItem(data[i]));
+    users.push(new UserItem(data[i]));
   }
     viewmodel.userList(users);
   
@@ -50,11 +104,11 @@ socket.on('check-answer', function(data){
 
 
 socket.on('score',function(data){
-    console.log("score"+JSON.stringify(data));
     var record=[];
+    viewmodel.leadboardList.removeAll();
     for (var i = 0; i < data.length; i++) {
-     var text=data[i].userId+"    "+data[i].correct;
-     var item=new leadboardItem(text);
+      var text=data[i].userId+':    '+data[i].correct;
+     var item=new LeadboardItem(text);
      record.push(item);     
      }
      viewmodel.leadboardList(record);
@@ -63,82 +117,11 @@ socket.on('score',function(data){
 });
  
   socket.on('history', function(data){
-    console.log('history',JSON.stringify(data));  
-     var item=new historyItem(JSON.stringify(data));  
+     console.log('history',JSON.stringify(data));  
+     var item=new HistoryItem(JSON.stringify(data));  
      var array=viewmodel.historyList();
      array.push(item);
      viewmodel.historyList(array);
   });
-
-
-
-function userItem(name) {
-    var self = this;
-    self.name = name;
-}
-
-function leadboardItem(record){
-      var self = this; 
-      self.item = record;
-}
-
-function historyItem(record) {
-    var self = this;
-    self.record = record;
-}
-
-
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-var  viewmodel= {
-    userName :ko.observable(''),
-    answerQA:ko.observable(''),
-    checkAnswer:ko.observable(''),
-    
-    historyList:ko.observableArray(),
-    leadboardList:ko.observableArray([]),
-    userList:ko.observableArray([]),
-    userFormArea:ko.observable(false),
-
-    historyArea:ko.observable(false),
-    mainArea:ko.observable(true),
-    checkAnswerArea:ko.observable(false),
-
-    addUser: function() {
-            console.log("new user's Name", viewmodel.userName());
-            socket.emit('new user', viewmodel.userName(),function(data){
-            //data true or false
-            if(data){
-                viewmodel.userFormArea(false);
-                viewmodel.mainArea(true);
-            }else{
-            }
-          });
-        },
-    submitAnswer:function() {
-        console.log("anserQA",this.answerQA());
-        socket.emit('submit-answer',{
-        'answerId': answerId,
-        'answer': this.answerQA(),
-        'userId': this.userName()
-    });
-        this.historyArea(true);
-        this.checkAnswerArea(false);
-    },
-    getQestion:function(){
-        socket.emit('new-round');
-        this.answerQA('');
-        this.checkAnswerArea(false)
-        this.historyArea(false);
-
-    }
-
-
-
-    
-};
-viewmodel.userFormArea(true);
-viewmodel.mainArea(false);
-
 // Activates knockout.js
 ko.applyBindings(viewmodel);
-
